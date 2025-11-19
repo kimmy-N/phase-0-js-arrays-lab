0d251fddad186750ada40d92d5740dbb0138e165
--add <168851515+nelly2040@users.noreply.github.com>
2025-11-19 18:26:11 +0300

    tests pass ed

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,6 +1,21 @@
 // Write your code here
+const products = ["Laptop", "Phone", "Headphones", "Monitor"];
 
+function logFirstProduct() {
+  console.log(products[0]);
+}
 
+function addProduct(product) {
+  products.push(product);
+}
+
+function updateProductName(pos, newName) {
+  products[pos] = newName;
+}
+
+function removeLastProduct() {
+  products.pop();
+}
 
 // Export the necessary parts for testing
 module.exports = {
